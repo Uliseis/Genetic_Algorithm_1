f2b57b7d058268e3d376f68f22d798750bc538b0
Uliseis <usbrgr@gmail.com>
2020-11-20 19:19:25 +0100

    CAMBIOS

--- group08/CrossoverOperator.py
+++ group08/CrossoverOperator.py
@@ -3,7 +3,8 @@ from abc import ABC, abstractmethod
 class CrossoverOperator(ABC):
     @abstractmethod
     def __init__(self):
-    
+        super.__init__
+        
     @abstractmethod
     def apply(self, genomas):
         pass
---
+++ group08/TwoPointOperator.py
@@ -0,0 +1,9 @@
+class TwoPointOperator(object):
+
+    def __init__(self):
+        super.__init__
+        
+    
+    def apply(self, genomas):
+        p1 = genomas.solucion.get
+        p2 = genomas.solucion.get
