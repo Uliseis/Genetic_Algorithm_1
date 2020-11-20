bccd5147f9db04d7c1f06c2d966a20cd19ce0d04
Jose Lopez Lopez <jose.llopez@alumnos.upm.es>
2020-11-20 20:04:07 +0100

    aa

--- group08/Genome.py
+++
@@ -1,9 +0,0 @@
-class Genome(solucion, fitness):
-    #Solucion candidata
-    solucion = list()
-    #Fitness de la solucion
-    fitness = 0
-    def __init__(self, solucion, fitness):
-        self.solucion = solucion
-        self.fitness = fitness
-
