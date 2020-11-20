3df441b2c44a97ce977a2d830e394a361366697d
Mihail <mssn9@outlook.com>
2020-11-20 20:19:55 +0100

    Population first gen

---
+++ group08/Population.py
@@ -0,0 +1,39 @@
+import sys as s
+class Population(Genome):
+    global population = list()
+    psize = 0
+    def __init__(self, psize):
+        self.psize = psize
+
+    def add(self,Genome):
+        if len(population)< self.psize
+            if Genome not in population:
+                population.append(Genome)
+        else:
+            return -1
+        return 0
+
+    def remove(self,toRemove):
+        if len(population) > 1
+            population.remove(toRemove)
+
+    def bestFitness(self,elem):
+        min = float('inf')
+        index = 0
+        for i in range(len(population)):
+            if(min < i.fitness):
+                min = i.fitness
+                index = i
+        return population[index]
+        
+    def sort(self,reverse = False):
+        if reverse:
+            population.sort(key = bestFitness, reverse = True)
+        else:
+            population.sort(key = bestFitness)
+        return population
+        
+    def replaceSol(self, oldSol, newSol):
+        i = population.index(oldSol)
+        population[i] = newSol
+        return i
