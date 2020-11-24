7552c64c1801ac9543ae3184f60009a5a4093262
Jose Lopez Lopez <jose.llopez@alumnos.upm.es>
2020-11-24 18:37:40 +0100

    no errores

--- group08/Operators/ReplacementOperator/ReplacementOperator.py
+++ group08/Operators/ReplacementOperator/ReplacementOperator.py
@@ -3,8 +3,8 @@ from abc import ABC, abstractmethod
 class SelectionOperator (ABC):
     @abstractmethod
     def __init__ (self):
-        super.__init__
-    
+        pass
+
     @abstractmethod
     def apply (self,population1, population2):
         pass
